745672fb34214442fa5b0da1892256e9f004d6c5
daynesohn <dayne.sohn@gmail.com>
2023-01-15 08:55:06 -0600

    fixing typo in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: `${process.env.CONTENTFUL_TOKEN}`,
+        accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
         spaceId: `${process.env.CONTENTFUL_SPACEID}`,
       },
     },
